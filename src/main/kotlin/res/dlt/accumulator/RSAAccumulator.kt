7cabb13a91c5fd93545f709d8f5f6777933a9d34
Allison Irvin <allirvin@au1.ibm.com>
2020-09-30 17:25:44 +1000

    Added the add function to add elements to the accumulator

--- src/main/kotlin/res/dlt/accumulator/RSAAccumulator.kt
+++ src/main/kotlin/res/dlt/accumulator/RSAAccumulator.kt
@@ -1,19 +1,21 @@
 package res.dlt.accumulator
 
+import arrow.core.Tuple2
 import java.math.BigInteger
 
 data class RSAAccumulator(
         val a: BigInteger,
-        private val a0: BigInteger,
-        private val p: BigInteger,
-        private val q: BigInteger,
-        private val n: BigInteger,
-        private val data: Map<BigInteger, BigInteger>
+        val a0: BigInteger,
+        val p: BigInteger,
+        val q: BigInteger,
+        val n: BigInteger,
+        val data: HashMap<BigInteger, BigInteger>
 ) {
-
     companion object {
         private const val RSA_KEY_SIZE = 3072
         private const val RSA_PRIME_SIZE = RSA_KEY_SIZE / 2
+        const val ACCUMULATED_PRIME_SIZE = 128
+        const val PRIME_CERTAINTY = 5
 
         fun newInstance(
                 seed1: Long = 1234567890123456789,
@@ -30,16 +32,26 @@ data class RSAAccumulator(
             val a0 = randomBigInteger(BigInteger.ZERO, n, seed3)
             val a = a0
 
-            return RSAAccumulator(a, a0, p, q, n, mapOf())
+            return RSAAccumulator(a, a0, p, q, n, hashMapOf())
         }
     }
 }
 
-fun add(x: BigInteger): BigInteger = TODO()
+// TODO: Fix this to include the new data map
+fun add(
+        accumulator: RSAAccumulator,
+        x: BigInteger
+): Tuple2<RSAAccumulator, BigInteger> = if (accumulator.data.containsKey(x)) {
+    Tuple2(accumulator, x)
+} else {
+    val (hashPrime, nonce) = hashToPrime(x, RSAAccumulator.ACCUMULATED_PRIME_SIZE)
+    val accumulatorValue = accumulator.a.modPow(hashPrime, accumulator.n)
+    accumulator.data[hashPrime] = nonce
+    val newAccumulator = accumulator.copy(a = accumulatorValue)
+    Tuple2(newAccumulator, x)
+}
 
 fun delete(x: BigInteger): BigInteger = TODO()
-
-fun batchUpdate(xs: List<BigInteger>): List<BigInteger> = TODO()
 
 fun createProof(x: BigInteger): Boolean = TODO()
 
--- src/main/kotlin/res/dlt/accumulator/Utils.kt
+++ src/main/kotlin/res/dlt/accumulator/Utils.kt
@@ -1,6 +1,8 @@
 package res.dlt.accumulator
 
+import com.google.common.hash.Hashing
 import arrow.core.Tuple2
+import com.google.common.io.BaseEncoding
 import java.math.BigInteger
 import kotlin.math.pow
 
@@ -15,25 +17,26 @@ tailrec fun generateTwoDistinctPrimes(
 ): Tuple2<BigInteger, BigInteger> = when {
     candidates == null -> {
         val newCandidates = Tuple2(
-            generateProbablePrime(bitLength, seed1),
-            generateProbablePrime(bitLength, seed2))
+                BigInteger.probablePrime(bitLength, java.util.Random(seed1)),
+                BigInteger.probablePrime(bitLength, java.util.Random(seed2)))
         generateTwoDistinctPrimes(bitLength, newCandidates, seed1, seed2)
     }
     candidates.a == candidates.b -> {
-        val newCandidates = Tuple2(candidates.a, generateProbablePrime(bitLength, seed2))
+        val newCandidates = Tuple2(
+                candidates.a,
+                BigInteger.probablePrime(bitLength, java.util.Random(seed2)))
         generateTwoDistinctPrimes(bitLength, newCandidates, seed1, seed2 + 1)
     }
     (candidates.a - candidates.b).toDouble() < bitLength.toDouble().pow(0.25) -> {
         candidates
     }
     else -> {
-        val newCandidates = Tuple2(candidates.a, generateProbablePrime(bitLength, seed2))
+        val newCandidates = Tuple2(
+                candidates.a,
+                BigInteger.probablePrime(bitLength, java.util.Random(seed2)))
         generateTwoDistinctPrimes(bitLength, newCandidates, seed1, seed2 + 1)
     }
 }
-
-fun generateProbablePrime(bitLength: Int, seed: Long): BigInteger =
-        BigInteger.probablePrime(bitLength, java.util.Random(seed))
 
 tailrec fun randomBigInteger(
         from: BigInteger,
@@ -48,5 +51,25 @@ tailrec fun randomBigInteger(
         randomNumber
     } else {
         randomBigInteger(from, until, seed + 1)
+    }
+}
+
+/**
+ * The hashToPrime function takes a BigInteger representation of an element
+ * to be added to the accumulator and hashes it together with a nonce until
+ * the resulting Big Integer is a prime number with a certain likelihood. This
+ * function uses the BigInteger.isProbablePrime method to test for primality,
+ * which under the hood uses the Miller–Rabin primality test.
+ */
+tailrec fun hashToPrime(
+        x: BigInteger,
+        bitLength: Int = 120,
+        candidateNonce: BigInteger = BigInteger.ZERO
+): Tuple2<BigInteger, BigInteger> {
+    val candidatePrime = BigInteger(Hashing.sha256().hashBytes((x + candidateNonce).toByteArray()).asBytes())
+    return if (candidatePrime.isProbablePrime(RSAAccumulator.PRIME_CERTAINTY)) {
+        Tuple2(candidatePrime, candidateNonce)
+    } else {
+        hashToPrime(x, bitLength, candidateNonce + BigInteger.ONE)
     }
 }
